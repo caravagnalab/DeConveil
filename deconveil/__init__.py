97e83b56536f5fc7efec41df2951880d8583d645
Katsiaryna Davydzenka <katsiarynadavydzenka@MBPdiKatsiaryna.homenet.telecomitalia.it>
2026-02-02 17:53:18 +0100

    update version

--- deconveil/__init__.py
+++ deconveil/__init__.py
@@ -3,5 +3,6 @@ from .inference import Inference
 from .default_inference import DefInference
 from .ds import deconveil_stats
 from .grid_search import grid_fit_shrink_beta
+from .nb_regression_fit import *
 
 
